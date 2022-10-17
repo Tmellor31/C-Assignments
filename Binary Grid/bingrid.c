d51021ea56ae26bfb8860b1189a4a41250c6b4cf
Tom Mellor <tommellor31@gmail.com>
2022-10-17 20:55:06 +0100

    Made solve board way nicer - added Pairs rule

--- Binary Grid/bingrid.c
+++ Binary Grid/bingrid.c
@@ -1,12 +1,15 @@
 #include "bingrid.h"
 
-//need to rename the row and col to row and col respectively, and then make sure the ordering is consistent - will change at end
+bool applyPairsToRow(board *brd, int row, int col);
+bool applyPairsToCol(board *brd, int row, int col);
+
+// need to rename the row and col to row and col respectively, and then make sure the ordering is consistent - will change at end
 
 void printboard(board *brd);
 // Retrieves the character from str that relates to row and col
 char setcell(char *str, int grid_size, int row, int col)
 {
-    return str[row + (grid_size * col)];
+    return str[col + (grid_size * row)];
 }
 
 bool str2board(board *brd, char *str)
@@ -20,7 +23,7 @@ bool str2board(board *brd, char *str)
         return false;
     }
 
-    brd->sz = strlen(str) / 2;
+    brd->sz = sqrt(strlen(str));
     // set the size of b2d
     // populate b2d
     int row, col;
@@ -42,73 +45,101 @@ void board2str(char *str, board *brd)
     }
 }
 
-bool solve_board(board *brd)
-{   bool haschanged = false;  
-    //Checks for 'Pairs' rule
-    printboard(brd);  
-    int row, col; 
+bool solve_board(board *brd) //Should return true if board is solvable 
+{
+    bool haschanged = false;
+    // Checks for 'Pairs' rule
+    printboard(brd);
+    int row, col;
     for (row = 0; row < brd->sz; row++) // Could use a dowhile loop instead of &&haschanged
     {
         for (col = 0; col < brd->sz; col++)
         {
-          if((brd->b2d[row][col] ==  1) && (brd->b2d[row][col+1] == 1)) 
-          {
-            brd->b2d[row+1][col] = 0;
-            brd->b2d[row+1][col+1] = 0;
-            haschanged = true;  
-            return true; 
-          }
-          if((brd->b2d[row+1][col] ==  1) && (brd->b2d[row+1][col+1] == 1) && (haschanged == false))  
-          {
-            brd->b2d[row+1][col] = 0;
-            brd->b2d[row+1][col+1] = 0;
-            haschanged = true;  
-            return true; 
-          } 
-          if((brd->b2d[row][col] ==  1) && (brd->b2d[row+1][col] == 1) && (haschanged == false))  
-          {
-            brd->b2d[row][col+1] = 1;
-            brd->b2d[row+1][col+1] = 1;
-            haschanged = true;  
-            return true; 
-          }
-          if((brd->b2d[row][col] ==  0) && (brd->b2d[row][col+1] == 0) && (haschanged == false))  
-          {
-            brd->b2d[row+1][col] = 1;
-            brd->b2d[row+1][col+1] = 1;
-            haschanged = true;  
-            return true;
-          }  
-          if((brd->b2d[row+1][col] ==  0) && (brd->b2d[row+1][col+1] == 0) && (haschanged == false))  
-          {
-            brd->b2d[row][col] = 1;
-            brd->b2d[row][col+1] = 1;
-            haschanged = true;  
-            return true;
-          }  
-          
-return false;    
-        } 
-    }   
+            applyPairsToRow(brd, row, col);
+            applyPairsToCol(brd, row, col);
+        }
+    }
+    return true;
+}
+
+char oppositeNumber(char x)
+{
+    if (x == ONE)
+    {
+        return ZERO;
+    }
+    if (x == ZERO)
+    {
+        return ONE;
+    }
+    else
+    {
+        return UNK;
+    }
+}
+
+bool applyPairsToRow(board *brd, int row, int col)
+{
+    /*These functions should eventually return true if cells have changed and false if not*/
+
+    if (brd->b2d[row][col] == brd->b2d[row][col + 1])
+    {
+        if ((col > 0) && (brd->b2d[row][col - 1] == UNK))
+        {
+            brd->b2d[row][col - 1] = oppositeNumber(brd->b2d[row][col]);
+        }
+        if ((col < brd->sz) && (brd->b2d[row][col + 2] == UNK))
+        {
+            brd->b2d[row][col + 2] = oppositeNumber(brd->b2d[row][col]);
+        }
+    }
+    return true;
+}
+
+bool applyPairsToCol(board *brd, int row, int col) // sort this outttttttt
+{
+    if (brd->b2d[row][col] == brd->b2d[row + 1][col])
+    {
+        if ((row > 0) && (brd->b2d[row - 1][col] == UNK))
+        {
+            brd->b2d[row - 1][col] = oppositeNumber(brd->b2d[row][col]);
+        }
+        if ((row < brd->sz) && (brd->b2d[row + 2][col] == UNK))
+        {
+            brd->b2d[row + 2][col] = oppositeNumber(brd->b2d[row][col]);
+            printf("Jay\n"); 
+        }
+    }
+    return true;
 }
 
 void printboard(board *brd)
 {
-    int i, j;
-    for (i = 0; i < brd->sz; i++)
+    int row, col;
+    for (row = 0; row < brd->sz; row++)
     {
-        for (j = 0; j < brd->sz; j++)
+        for (col = 0; col < brd->sz; col++)
         {
-            printf("row = %i, col = %i, %c\n", i, j, brd->b2d[i][j]);
+            printf("row = %i, col = %i, %c\n", row, col, brd->b2d[row][col]);
         }
     }
 }
 
 void test(void)
 {
+    //assert str2board with a string of 12 characters, it should fail due to sqrt. b.sz should equal 0. 
+    assert(oppositeNumber(ONE) == ZERO);
+    assert(oppositeNumber(ZERO) == ONE);
+    assert(oppositeNumber(UNK) == UNK);
     board b;
     b.sz = 0;
     char str[0];
+    assert(str2board(&b, ".11.00..0......."));
+    assert(b.sz == 4); 
+    assert(applyPairsToCol(&b, 1, 0) == true);
+    assert(b.b2d[0][0] == ONE);
+    assert(b.b2d[3][0] == ONE);
+    printboard(&b);
     assert(str2board(&b, ".0.."));
     assert(b.sz == 2);
     assert(b.b2d[0][0] == UNK);
